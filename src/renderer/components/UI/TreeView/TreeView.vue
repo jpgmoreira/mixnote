8a61a2f83944c4d9fa2e5d7c94d1f28b60d4db6a
joao <jpgmoreira19@gmail.com>
2025-12-25 16:24:04 -0300

    .

--- src/renderer/components/UI/TreeView/TreeView.vue
+++ src/renderer/components/UI/TreeView/TreeView.vue
@@ -726,20 +726,4 @@
   .cursor-not-allowed {
     cursor: not-allowed;
   }
-
-  /* --- Transitions --- */
-
-  /* Fade: */
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.25s ease;
-  }
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
-  .fade-enter-to,
-  .fade-leave-from {
-    opacity: 1;
-  }
 </style>
